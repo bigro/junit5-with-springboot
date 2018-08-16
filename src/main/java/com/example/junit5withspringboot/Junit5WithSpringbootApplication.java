9261dd7e1e38871450da5e796c7419555dbb5e0c
Yuuki Ooguro <kitkat.jaja@gmail.com>
2018-08-16 16:59:33 +0900

    first commit

---
+++ src/main/java/com/example/junit5withspringboot/Junit5WithSpringbootApplication.java
@@ -0,0 +1,12 @@
+package com.example.junit5withspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Junit5WithSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Junit5WithSpringbootApplication.class, args);
+	}
+}
